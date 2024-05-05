13a920dcdbbfbab9139f8efca30cc98928c23b95
HaniSinghCse <hanisinghcse2020@gmail.com>
2024-05-05 20:47:21 +0530

    update

---
+++ app/src/main/java/com/example/hanisingh/hospitalmanagement/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.hanisingh.hospitalmanagement;
+
+import android.content.ClipData;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.hanisingh.hospitalmanagement.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private SearchView searchView;
+    private ActivityMainBinding binding;
+    private  list<Item> itemList; // ye new data dalenge tho jaega
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Search view code here
+        searchView = findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                fileList(newText);
+                return true;
+            }
+        }); // search view code end
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+    }
+// search view code here
+    // ye section rest h q ki av data nii dale h
+    private void fileList(String Text) {
+        List<Item> filterList = new ArrayList<>();
+        for(Item item : itemList)
+        {
+    }
+
+}
